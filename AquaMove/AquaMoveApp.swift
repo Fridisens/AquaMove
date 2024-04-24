dc883a01cf11e5dbfb23b68d30fd89af1087842c
Fridisens <fridisens@gmail.com>
2024-04-24 09:34:13 +0200

    Initial Commit

---
+++ AquaMove/AquaMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaMoveApp.swift
+//  AquaMove
+//
+//  Created by Frida Dahlqvist on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct AquaMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AquaMove/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AquaMove
+//
+//  Created by Frida Dahlqvist on 2024-04-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
