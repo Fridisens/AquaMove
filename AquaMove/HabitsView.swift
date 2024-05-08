e98a1c7bc8e4f820ef34bea2926775a291ece944
Fridisens <fridisens@gmail.com>
2024-05-08 07:46:52 +0200

    gitignore changes

--- AquaMove/HabitsView.swift
+++ AquaMove/HabitsView.swift
@@ -17,40 +17,41 @@ struct HabitsView: View {
     let db = Firestore.firestore()
     
     var body: some View {
-            NavigationStack {
-                List {
-                    ForEach(viewModel.habits, id: \.id) { habit in
-                        VStack(alignment: .leading) {
-                            Text(habit.name)
-                                .font(.headline)
-                            Text(habit.description)
-                                .font(.subheadline)
-                            Text(habit.days)
-                            if let waterHabit = habit as? WaterIntakeHabit {
-                                Text("Mål: \(waterHabit.goal, specifier: "%.0f") liter")
-                            } else if let wellnessHabit = habit as? WellnessHabit {
-                                Text("Träningslängd: \(wellnessHabit.sessionLength, specifier: "%.0f") minuter")
-                            }
-                        }
-                    }
-                    .onDelete(perform: viewModel.deleteHabit)
-                    }
-                }
-                .navigationTitle("Lista på vanor")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: {
-                            showingAddHabit = true
-                        }) {
-                            Image(systemName: "plus")
-                                .font(.system(size: 24)) 
+        NavigationStack {
+            List {
+                ForEach(viewModel.habits, id: \.id) { habit in
+                    VStack(alignment: .leading) {
+                        Text(habit.name)
+                            .font(.headline)
+                        Text(habit.description)
+                            .font(.subheadline)
+                        Text(habit.days)
+                        if let waterHabit = habit as? WaterIntakeHabit {
+                            Text("Mål: \(waterHabit.goal, specifier: "%.0f") liter")
+                        } else if let wellnessHabit = habit as? WellnessHabit {
+                            Text("Träningslängd: \(wellnessHabit.sessionLength, specifier: "%.0f") minuter")
                         }
                     }
                 }
-                .sheet(isPresented: $showingAddHabit) {
-                    AddHabitView(habits: $viewModel.habits)
+                .onDelete(perform: viewModel.deleteHabit)
+                
+            }
+            .navigationTitle("Lista på vanor")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        showingAddHabit = true
+                    }) {
+                        Image(systemName: "plus")
+                            .font(.system(size: 24))
+                    }
                 }
             }
+            .sheet(isPresented: $showingAddHabit) {
+                AddHabitView(habits: $viewModel.habits)
+            }
         }
-
-
+    }
+    
+    
+}
