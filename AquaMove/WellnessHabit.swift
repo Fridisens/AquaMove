fd85bb9b082af69499f8e6272a2e7547ac4bc80e
Fridisens <fridisens@gmail.com>
2024-05-08 09:14:02 +0200

    removed lines and cleaned code

--- AquaMove/WellnessHabit.swift
+++ AquaMove/WellnessHabit.swift
@@ -12,28 +12,22 @@ class WellnessHabit : Habit {
     var sessionLength: Double
     var unit: String = "minuter"
    
-
     init(name: String, description: String, sessionLength: Double, days: String, time: Date) {
         self.sessionLength = sessionLength
         
-        
         super.init(name: name, description: description, days: days, time: time)
   
-    
     }
-    
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         sessionLength = try container.decode(Double.self, forKey: .sessionLength)
         try super.init(from: decoder)
     }
-    
     override func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(sessionLength, forKey: .sessionLength)
         try super.encode(to: encoder)
     }
-    
     private enum CodingKeys: String, CodingKey {
         case sessionLength
     }
